3206196b6df46e5f894caf4bc648dc255876b62a
Monsterdiscord <99562167+Monsterdiscord@users.noreply.github.com>
2022-02-20 17:50:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,38 @@ bot5= commands.Bot(command_prefix='!')
 
 bot.author_id = enter id here  # Change to your discord id!!!
 
-@bot.event 
-async def on_ready():  # When the bot is ready
-    print("I'm in")
-    print(bot.user)  # Prints the bot's username and identifier
+
+bot.author_id = enter id here  # Change to your discord id!!!
+
+@bot.event
+async def on_ready():
+  game = discord.Game(f"made by monster")
+  await bot.change_presence(status=discord.Status, activity=game)
+  print(f'logged in as {bot.user}')
+
+@bot2.event
+async def on_ready():
+  game = discord.Game(f"made by monster")
+  await bot2.change_presence(status=discord.Status, activity=game)
+  print(f'logged in as {bot2.user}')
+
+@bot3.event
+async def on_ready():
+  game = discord.Game(f"made by monster")
+  await bot3.change_presence(status=discord.Status, activity=game)
+  print(f'logged in as {bot3.user}')
+
+@bot4.event
+async def on_ready():
+  game = discord.Game(f"made by monster")
+  await bot4.change_presence(status=discord.Status, activity=game)
+  print(f'logged in as {bot4.user}')
+
+@bot5.event
+async def on_ready():
+  game = discord.Game(f"made by monster")
+  await bot4.change_presence(status=discord.Status, activity=game)
+  print(f'logged in as {bot5.user}')
 
 
 
@@ -31,9 +59,9 @@ keep_alive()  # Starts a webserver to be pinged.
 loop = asyncio.get_event_loop()
 loop.create_task(bot.start('enter token here',bot=False))
 loop.create_task(bot2.start('enter token here',bot=False))
-#loop.create_task(bot3.start('enter token here'.bot=False))
-#loop.create_task(bot4.start('enter token here',bot=False))
-#loop.create_task(bot5.start('enter token here',bot=False))
+loop.create_task(bot3.start('enter token here'.bot=False))
+loop.create_task(bot4.start('enter token here',bot=False))
+loop.create_task(bot5.start('enter token here',bot=False))
 # you can add more if you wish you just need to copy loop.create_task(bot5.start('enter token here')) and bot5 = commands.Bot(command_prefix='!'), and change 
 # and change the bot5 to bot6 and so forth with how many more bots you want to add
 loop.run_forever()
